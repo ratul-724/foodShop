9dec0eeb1aceff2d58b91c960e8714fe0d72c03a
rhratul306@gmail.com <RaTuL HasAn>
2025-01-05 03:04:39 +0600

    change

--- js/user.js
+++ js/user.js
@@ -1,8 +1,8 @@
+// code for hide and show the login and register form
 let loginBtn = document.getElementById("goToSignIn");
 let loginDirectory = document.getElementById("login");
 let signInBtn = document.getElementById("goToRegister");
 let signInDirectory = document.getElementById("register");
-
 
 signInBtn.addEventListener('click', function() {
     signInDirectory.classList.remove('hide');
@@ -12,3 +12,6 @@ loginBtn.addEventListener('click', function() {
     loginDirectory.classList.remove('hide');
     signInDirectory.classList.add('hide');
 });
+// code for hide and show the login and register form
+
+// code for login and register form
